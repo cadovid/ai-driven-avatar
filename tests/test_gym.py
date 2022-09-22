c77010ee20dbd6636f384a72192e3a4dd1e99b94
david camino perdones <david.camino.perdones@gmail.com>
2022-09-22 11:48:00 +0200

    update test to opengym env

--- tests/test_gym.py
+++ tests/test_gym.py
@@ -1,19 +1,20 @@
 import pytest
 
 from src.opengym.__main__ import GymGame
-from src.rl_algorithms.__main__ import RLAlgorithm
 
 
 @pytest.mark.gym_env
 def test_gym_environment(example_actions):
     env = GymGame()
-    RLAlgorithm(env)
+    env.reset()
     while example_actions:
             # Get valid actions space
             env.get_valid_actions()
 
             # Take test action
             action = example_actions.popleft()
+            if (action == 6 or action == 5) and (action not in env._valid_actions):
+                action = 8
             assert action in env._valid_actions
 
             # Run action
@@ -21,10 +22,10 @@ def test_gym_environment(example_actions):
             
             # Render the game (slow the process in order not to see a crazy fast video)
             env.render()
-            
+
             # Check end of the episode conditions
             if done == True:
-                assert env.episodic_step == 85
+                assert env.episodic_step == 147 or env.episodic_step == 148
                 break
     
     env.close()
