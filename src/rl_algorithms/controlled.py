3329033ef2c5e41063c5f0eb4cec1a0e3b44b7db
david camino perdones <david.camino.perdones@gmail.com>
2022-10-21 13:12:19 +0200

    added memory mechanic and updated movement mechanics with stochastic movement

--- src/rl_algorithms/controlled.py
+++ src/rl_algorithms/controlled.py
@@ -1,7 +1,7 @@
 import time
 
 
-from random import sample
+from random import choice, random
 from src.pygame.settings import CONSUMABLES, HARMFUL_ITEMS, PICKABLE_ITEMS, TILESIZE
 from src.utils.actions import Action
 from src.utils.pathfinding import a_star_algorithm
@@ -14,6 +14,15 @@ class ControlledAlgorithm():
         self.env = environment
         self.state = self.env.reset()
     
+    def add_memento_to_memory(self, avatar, memento):
+        avatar.add_to_memory(memento)
+
+    def store_event(self, avatar, event):
+        for object, _ in self.env.game.sight_objects.items():
+            if object.type == event and event not in avatar.memory:
+                self.add_memento_to_memory(avatar, {event: (int(object.pos.x / TILESIZE), int(object.pos.y / TILESIZE))})
+                print(f"[Memory track] Stored event < '{event}': {(int(object.pos.x / TILESIZE), int(object.pos.y / TILESIZE))} >")
+
     def find_and_move_towards_closest_object(self, avatar, focus_on=None):
         if focus_on is None:
             # Ignore not pickable objects (create a shallow copy of the vision)
@@ -37,10 +46,16 @@ class ControlledAlgorithm():
                 action = sequence_actions.popleft()
             else:
                 print("A* algorithm did not find an optimal path. Random movement executed")
-                action = sample([action for action in self.env._valid_actions if action in [0, 1, 2, 3]], 1)[0]
+                action = choice([action for action in self.env._valid_actions if action in [Action.LEFT.value, Action.RIGHT.value, Action.UP.value, Action.DOWN.value]])
         
-        # If not objects at sight, then move in a continuous direction
+        # If not objects at sight, then move in a continuous direction until find a wall or an object is seen
+        # At each step of the continuous movement, a certain randomness is created to allow for stochastic 
+        # movements
         else:
+            valid_action_mov = []
+            for action in self.env._valid_actions:
+                if action in [Action.LEFT.value, Action.RIGHT.value, Action.UP.value, Action.DOWN.value]:
+                    valid_action_mov.append(action)
             if self.last_action is not None and self.last_action not in self.env._valid_actions and self.last_action in [Action.LEFT.value, Action.RIGHT.value, Action.UP.value, Action.DOWN.value]:
                 # Restrictions: Avoid going the opposite direction (where you came from) if possible
                 restrictions = {str(Action.RIGHT.value): Action.LEFT.value,
@@ -48,20 +63,22 @@ class ControlledAlgorithm():
                                 str(Action.DOWN.value): Action.UP.value,
                                 str(Action.UP.value): Action.DOWN.value
                                 }
-                valid_action_mov = []
-                for action in self.env._valid_actions:
-                    if action in [Action.LEFT.value, Action.RIGHT.value, Action.UP.value, Action.DOWN.value]:
-                        valid_action_mov.append(action)
                 if len(valid_action_mov) > 1:
                     action = restrictions[str(self.last_action)]
                     while action == restrictions[str(self.last_action)]:
-                        action = sample([action for action in self.env._valid_actions if action in [Action.LEFT.value, Action.RIGHT.value, Action.UP.value, Action.DOWN.value]], 1)[0]
+                        action = choice([action for action in self.env._valid_actions if action in [Action.LEFT.value, Action.RIGHT.value, Action.UP.value, Action.DOWN.value]])
                 else:
-                    action = sample([action for action in self.env._valid_actions if action in [Action.LEFT.value, Action.RIGHT.value, Action.UP.value, Action.DOWN.value]], 1)[0]
+                    action = choice([action for action in self.env._valid_actions if action in [Action.LEFT.value, Action.RIGHT.value, Action.UP.value, Action.DOWN.value]])
             elif self.last_action is not None and self.last_action in self.env._valid_actions and self.last_action in [Action.LEFT.value, Action.RIGHT.value, Action.UP.value, Action.DOWN.value]:
                 action = self.last_action
+                if random() < 0.1:
+                    # Allow stochastic chance of change direction
+                    if action in [Action.RIGHT.value, Action.LEFT.value] and len(valid_action_mov) > 2:
+                        action = choice([action for action in self.env._valid_actions if action in [Action.UP.value, Action.DOWN.value]])
+                    elif action in [Action.DOWN.value, Action.UP.value] and len(valid_action_mov) > 2:
+                        action = choice([action for action in self.env._valid_actions if action in [Action.LEFT.value, Action.RIGHT.value]])
             else:
-                action = sample([action for action in self.env._valid_actions if action in [Action.LEFT.value, Action.RIGHT.value, Action.UP.value, Action.DOWN.value]], 1)[0]
+                action = choice([action for action in self.env._valid_actions if action in [Action.LEFT.value, Action.RIGHT.value, Action.UP.value, Action.DOWN.value]])
         return action
 
     def run(self):
@@ -80,6 +97,10 @@ class ControlledAlgorithm():
 
             # Rules to perform actions
             for avatar in self.env.game.avatar_sprites:
+                # Set events to store in memory
+                self.store_event(avatar, 'water-dispenser')
+
+                # Set rules according to internal drives
                 if avatar.drives.internal_state == 'satisfied':
                     if bool(self.env.game.hitted_object) and (self.env.game.hitted_object.type in PICKABLE_ITEMS): # If on object, try something
                         if len(avatar.inventory) <= 4:
@@ -108,7 +129,19 @@ class ControlledAlgorithm():
                         if bool(self.env.game.hitted_object) and (self.env.game.on_water_source):
                             action = Action.DRINK.value
                         else:
-                            action = self.find_and_move_towards_closest_object(avatar, focus_on='water-dispenser')
+                            if 'water-dispenser' in avatar.memory:
+                                print("I remember a water-dispenser!")
+                                water_source_x, water_source_y = avatar.memory['water-dispenser']
+                                start = self.env.game.graph_map[int(avatar.pos.y / TILESIZE)][int(avatar.pos.x / TILESIZE)]
+                                end = self.env.game.graph_map[water_source_y][water_source_x]
+                                sequence_actions = a_star_algorithm(self.env.game.graph_map, start, end)
+                                if sequence_actions:
+                                    action = sequence_actions.popleft()
+                                else:
+                                    print("A* algorithm did not find an optimal path. Random movement executed")
+                                    action = choice([action for action in self.env._valid_actions if action in [Action.LEFT.value, Action.RIGHT.value, Action.UP.value, Action.DOWN.value]])
+                            else:
+                                action = self.find_and_move_towards_closest_object(avatar, focus_on='water-dispenser')
                     else:
                         if bool(self.env.game.hitted_object) and (self.env.game.hitted_object.type == 'cup'):
                             if len(avatar.inventory) <= 4:
